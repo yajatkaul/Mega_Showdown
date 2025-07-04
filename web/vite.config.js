a83a8ba7cd75bc7521b0d878abe6d38879713492
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2025-07-04 20:03:08 +0530

    DOCOSCCOSCOSCOSOCOSC

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "../build",
+    outDir: "../docs",
   },
 });
