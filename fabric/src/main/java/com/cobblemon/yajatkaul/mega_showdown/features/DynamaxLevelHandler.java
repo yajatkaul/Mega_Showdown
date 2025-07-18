901eb9fd7c3c9c5af3f6b8c56b2f8521b3f98918
BlackBeltPanda <dev@justpanda.dev>
2025-07-17 20:38:08 -0400

    Cleanup DynamaxLevelHandler and simplify logic for use in SummaryCompanionMixin#onOpen

--- fabric/src/main/java/com/cobblemon/yajatkaul/mega_showdown/features/DynamaxLevelHandler.java
+++ fabric/src/main/java/com/cobblemon/yajatkaul/mega_showdown/features/DynamaxLevelHandler.java
@@ -1,35 +1,15 @@
 package com.cobblemon.yajatkaul.mega_showdown.features;
 
 import com.cobblemon.mod.common.api.pokemon.feature.IntSpeciesFeature;
-import com.cobblemon.mod.common.net.messages.client.pokemon.update.SpeciesFeatureUpdatePacket;
 import com.cobblemon.mod.common.pokemon.Pokemon;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 public class DynamaxLevelHandler {
-    public static void update(Pokemon pokemon, ServerPlayerEntity player) {
-        if (pokemon.getSpecies().getDynamaxBlocked()) return;
-
-        final IntSpeciesFeature dmaxLevel;
-        IntSpeciesFeature existing = pokemon.getFeature("dynamax_level");
-        if (existing == null) {
-            dmaxLevel = new IntSpeciesFeature("dynamax_level", pokemon.getDmaxLevel());
-            pokemon.getFeatures().add(dmaxLevel);
-            pokemon.updateAspects();
-        } else {
-            existing.setValue(pokemon.getDmaxLevel());
-            dmaxLevel = existing;
-        }
-        pokemon.notify(new SpeciesFeatureUpdatePacket(() -> pokemon, pokemon.getSpecies().resourceIdentifier, dmaxLevel));
-
-        if (player != null) {
-            // Delay to avoid sending the packet before the client's registered the Pokémon in their party
-            player.server.execute(() ->
-                    new SpeciesFeatureUpdatePacket(() -> pokemon, pokemon.getSpecies().resourceIdentifier, dmaxLevel).sendToPlayer(player)
-            );
-        }
-    }
+    private static final String FEATURE_NAME = "dynamax_level";
 
     public static void update(Pokemon pokemon) {
-        update(pokemon, null);
+        if (pokemon == null || pokemon.getSpecies().getDynamaxBlocked()) return;
+
+        pokemon.getFeatures().removeIf(feature -> feature.getName().equals(FEATURE_NAME));
+        pokemon.getFeatures().add(new IntSpeciesFeature(FEATURE_NAME, pokemon.getDmaxLevel()));
     }
 }
