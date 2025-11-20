372242fe6ebf6746610572d542167739d3399d4e
Treynami <77269031+BlackStarG5@users.noreply.github.com>
2025-11-20 13:22:09 -0500

    Effect's For Mons
    - Added Particle effects for:
    Deoxys (anime show'd in vc)
    Rotom (anime show'd in vc)
    
    - Added Pikachu-Original
    - Added Pikachu-Hoenn
    - Added Pikachu-Sinnoh
    - Added Pikachu-Unova
    - Added Pikachu-Kalos
    - Added Pikachu-Alola
    - Added Pikachu-World
    - Name change in the form "Pop-Star" of Pikachu [Before: PopStar] [After: Pop-Star] Now, if you are referring to the showdown
    - Name change in the form "Rock-Star" of Pikachu [Before: RockStar] [After: Rock-Star] Now, if you are referring to the showdown
    - Fixed Pikachu-Alola-Bias (in the past he cant use the Pikanium-Z)
    - Fixed Pikachu-World (now he can use Pikashunium-Z)
    - Added aspect lines for Pikachu wearing a cap (so that they can change form).
    - [Pikachu - Partner] - Aspect: "cosmetic_item-pewter_crunchies"
    - [Pikachu - Original] - Aspect: "cosmetic_item-rage_candy_bar"
    - [Pikachu - Hoenn] - Aspect: "cosmetic_item-lava_cookie"
    - [Pikachu - Sinnoh] - Aspect: "cosmetic_item-old_gateau"
    - [Pikachu - Unova] - Aspect: "cosmetic_item-casteliacone"
    - [Pikachu - Kalos] - Aspect: "cosmetic_item-lumiose_galette"
    - [Pikachu - Alola] - Aspect: "cosmetic_item-big_malasada"
    - [Pikachu - World] - Aspect: "cosmetic_item-smoked_tail_curry"
    - Added the old Pikachu Animations
    - Added the old Pikachu Poser
    - Cosplay pikachus (not the cosplay form) user the old animations and poser
    - Resolver organized
    
    Region Bias MSD texture pack Changelog:
    - Now Cosplay Pikachu have a Brown Tail
    - Now Pikachu Gigantamax have Brown Ears
    - Everything now is just one Resolver

---
+++ common/src/main/resources/data/mega_showdown/showdown/held_items/pikaniumz.js
@@ -0,0 +1,11 @@
+{
+	name: "Pikanium Z",
+	spritenum: 649,
+	onTakeItem: false,
+	zMove: "Catastropika",
+	zMoveFrom: "Volt Tackle",
+	itemUser: ["Pikachu", "Pikachu-Alola-Bias"],
+	num: 794,
+	gen: 7,
+	isNonstandard: "Past"
+}
